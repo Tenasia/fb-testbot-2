b1d35f568b74788a5337660ad5100068fb1a79b2
Tenasia <alvicviojan@gmail.com>
2021-11-30 19:02:41 +0800

    Update fb-testbot-2.py

--- fb-testbot-2.py
+++ fb-testbot-2.py
@@ -15,7 +15,7 @@ def process_message(text):
     thanks_response = ["You're welcome, happy to help 😊", "No problem, anytime!", "Happy to serve 😄", "My pleasure. If you have anymore concerns, don't hesitate to ask the people on this page 🙂"]
     number_reply = ["Here is the link that will hopfully answer your complaint.", "I hope the link below will help answer your complaint.", "Below is the link that will hopefully answer the complaint you have requested."]
     about_bot = ["Oh me?", "I see that you want to know more of me.", "You're curious about me, aren't you?", "I'm kind of new here so I understand why you asked."]
-
+    title_question = ["What is the activities for this week?", "Where else can I submit my concerns?", "Why were you made?", "Where can I learn more about PUP?", "Where can I request my school documents?", "Is there a place where I can access a library of materials for class?", "How can I get straight 1's on my subjects?"]
     if formatted_message == "1" or formatted_message == "one" or formatted_message == "first" or formatted_message == "1st":
         response = f"{random.choice(number_reply)} \n\nClass G-Drive:\n(http://bit.ly/SA-TechDrive)\n\nSchool Handbook\n(https://drive.google.com/file/d/0B1BuDAuN0r8SX1BWX2NSN3FURzg/view?usp=drivesdk&resourcekey=0-oi8lUy9PCFysh0FDyL5ipw)\n\n School Documents:\nItech Document Request Form\n(https://bit.ly/2CA28vk)\nUniversity Document Request Form\n(https://odrs.pup.edu.ph/)\nDownloadable Forms\n(https://www.pup.edu.ph/downloads/students/)\n\n If you see that your question is not on the list, please comment on the page for assistance."
     elif formatted_message == "2":
@@ -29,11 +29,11 @@ def process_message(text):
     elif formatted_message == "6":
         response =f"{random.choice(number_reply)}\n\nYou don't.   "
     elif formatted_message == "7":
-        response =f"{random.choice(number_reply)}\n\nYou don't.   "
+        response =f"{title_question.iloc[6]}\n\nYou don't.   "
     elif formatted_message == "thank you" or formatted_message == "thankyou" or formatted_message == "thanks" or formatted_message == "ty" or formatted_message == "thx" or formatted_message == "salamat" or formatted_message == "salamuch" or formatted_message == "matsala" or formatted_message == "arigato" or formatted_message == "arigathanks":
         response = f"{random.choice(thanks_response)}"
     else:
-        response = f"{random.choice(greetings)} I'm ASH your Automated Student Helper, how may I help you today? Please type in the number of your choice:\n\n[1] What is the activities for this week?\n[2] Where else can I submit my concerns?\n[3] Why were you made?\n[4]Where can I learn more about PUP?\n[5]Where can I request my school documents?\n[6]Is there a place where I can access a library of materials for class?\n[7]How can I get straight 1's on my subjects?\n\n(Note: I'm just a simple bot so I'll keep repeating this response when you enter in a random text even if I don't understand it.)"
+        response = f"{random.choice(greetings)} I'm ASH your Automated Student Helper, how may I help you today? Please type in the number of your choice:\n\n[1] What is the activities for this week?\n[2] Where else can I submit my concerns?\n[3] Why were you made?\n[4] Where can I learn more about PUP?\n[5] Where can I request my school documents?\n[6] Is there a place where I can access a library of materials for class?\n [7] How can I get straight 1's on my subjects?\n\n(Note: I'm just a simple bot so I'll keep repeating this response when you enter in a random text even if I don't understand it.)"
     return response
 
 #We will receive messages that Facebook sends our bot at this endpoint
